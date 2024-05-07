8867f757726f185dbc139242168ace7c4533da8c
RonHimmel <ronhimmel01@gmail.com>
2024-05-07 16:01:26 +0200

    last version

--- app/src/main/java/de/fra_uas/fb2/mobiledevices/bordasvotingmethod/MainActivity.kt
+++ app/src/main/java/de/fra_uas/fb2/mobiledevices/bordasvotingmethod/MainActivity.kt
@@ -1,8 +1,10 @@
 package de.fra_uas.fb2.mobiledevices.bordasvotingmethod
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Switch
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -11,6 +13,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,12 +28,15 @@ class MainActivity : AppCompatActivity() {
         val savedOptions = intent.getStringArrayListExtra("options") ?: emptyList<String>()
         val savedTextOptions = intent.getStringExtra("textOptions")
         val savedNumberVotes = intent.getIntExtra("numberVotes", 0)
+        val savedPairList = intent.getStringExtra("pairList")
 
         val addVoteButton: Button = findViewById(R.id.ButtonAddVote)
         val intOptions: EditText = findViewById(R.id.EditTextOptionsNumber)
         val textOptions : EditText = findViewById(R.id.EditTextOptions)
         val startOverButton: Button = findViewById(R.id.ButtonStartOver)
         val numberOfVotes: TextView = findViewById(R.id.TextViewVotesNumber)
+        val resultVotes : TextView = findViewById(R.id.TextViewResults)
+        val switchResults : Switch = findViewById(R.id.SwitchResults)
 
 
         numberOfVotes.text = savedNumberVotes.toString()
@@ -47,7 +53,7 @@ class MainActivity : AppCompatActivity() {
             val numberInput = intOptions.text.toString()
             if (numberInput.isNotEmpty()&&numberInput.toInt()>1&&numberInput.toInt()<11) {
                 val numberOptions = numberInput.toInt()
-                val numberVotes = numberOfVotes.text.toString().toInt() + 1
+                val numberVotes = numberOfVotes.text.toString().toInt() + 1                         //adds 1 to the voting counter
                 val intent = Intent(this, MainActivity2::class.java)
                 val inputText = textOptions.text.toString()
                 val options = inputText.split(",").map { it.trim() }                        //splits the string into the different options using the split method with ","
@@ -61,6 +67,22 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Please enter a number from 2 to 10", Toast.LENGTH_SHORT).show()
             }
         }
+
+        switchResults.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked&&numberOfVotes.text!="0") {
+                /*var results = ""
+                for (i in 0 until savedNumberOptions) {
+                    results = if (i < savedOptions.size && savedOptions[i] != "") {
+                        results + savedOptions[i] + "\n"
+                    } else {
+                        results + "Option ${i + 1}" + "\n"
+                    }
+                    resultVotes.text = results
+                }*/
+                resultVotes.text = savedPairList
+            }else resultVotes.text = ""
+        }
+
     }
 
 }
--- app/src/main/java/de/fra_uas/fb2/mobiledevices/bordasvotingmethod/MainActivity2.kt
+++ app/src/main/java/de/fra_uas/fb2/mobiledevices/bordasvotingmethod/MainActivity2.kt
@@ -12,8 +12,6 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity2 : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -30,36 +28,86 @@ class MainActivity2 : AppCompatActivity() {
         val savedNumberVotes = intent.getIntExtra("numberVotes", 0)
 
         val confirmVoteButton: Button = findViewById(R.id.ButtonConfirmVote)
+        val cancelVoteButton: Button = findViewById(R.id.ButtonCancelVote)
         val seekBarsLayout: LinearLayout = findViewById(R.id.seekBarsLayout)
+        val voteScreen: TextView = findViewById(R.id.textViewBordaVoteDynamic)
 
-        confirmVoteButton.setOnClickListener {
+        var updatedListString = ""
+        val seekBarValues = IntArray(numberOptions)
+        val seekBar = ArrayList<SeekBar>(numberOptions)
+
+
+        fun intentions(a: Int){
             val intent = Intent(this, MainActivity::class.java)
             intent.putExtra("numberOptions", numberOptions)
             intent.putStringArrayListExtra("options", ArrayList(options))
             intent.putExtra("textOptions", text)
-            intent.putExtra("numberVotes", savedNumberVotes )
+            if(a==1) {intent.putExtra("numberVotes", savedNumberVotes )
+            } else intent.putExtra("numberVotes", savedNumberVotes -1)
+            intent.putExtra("pairList", updatedListString)
             startActivity(intent)
         }
 
-        val cancelVoteButton: Button = findViewById(R.id.ButtonCancelVote)
+        fun calculateResults(){
+            val tripleList = mutableListOf(
+                Triple(if(options[0]!="")options[0]else "Option 1", seekBarValues[0], 0)
+            )
+            for (j in 0 until numberOptions) {
+                if(j in 1 until numberOptions){
+                    if(j < options.size && options[j] != "") {
+                        tripleList.add(Triple(options[j], seekBarValues[j], 0))
+                    }else{
+                        tripleList.add(Triple("Option ${j+1}", seekBarValues[j], 0))
+                    }
+                }
+            }
+            val sortedList = tripleList.sortedByDescending { it.second }.toMutableList()
+            for(y in 0 until numberOptions){
+                val existingPair = sortedList[y]
+                val updatedPair = Triple(existingPair.first, existingPair.second, numberOptions-y-1)
+                sortedList[y] = updatedPair
+            }
+            updatedListString = sortedList.joinToString(separator = "\n") { "${it.first} -> ${it.third}" }
+            voteScreen.text = updatedListString
 
+        }
+
+        confirmVoteButton.setOnClickListener {
+            intentions(1)
+        }
         cancelVoteButton.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            intent.putExtra("numberOptions", numberOptions)
-            intent.putStringArrayListExtra("options", ArrayList(options))
-            intent.putExtra("textOptions", text)
-            intent.putExtra("numberVotes", savedNumberVotes -1)
-            startActivity(intent)
+            intentions(0)
         }
 
         for (i in 0 until numberOptions) {
-            val seekBar = SeekBar(this)
-            seekBar.progress = 0
+            val bar = SeekBar(this)
+            bar.progress = 0
+            seekBarValues[i] = bar.progress
+            seekBar.add(bar)
             val seekBarName = if (i < options.size&&options[i]!="") options[i] else "Option ${i+1}"
             val textView = TextView(this)
             textView.text = seekBarName
             seekBarsLayout.addView(textView)
-            seekBarsLayout.addView(seekBar)
+            seekBarsLayout.addView(bar)
+
+            val listener = object : SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                    // Store the progress value in the array
+                    seekBarValues[i] = progress
+                    calculateResults()
+                }
+
+                override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                    // Handle start tracking touch
+                }
+
+                override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                    // Handle stop tracking touch
+                }
+            }
+            // Add the listener to the SeekBar
+            bar.setOnSeekBarChangeListener(listener)
         }
     }
+
 }
